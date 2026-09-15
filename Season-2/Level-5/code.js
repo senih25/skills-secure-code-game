6cc9352ca2c0c04221941c11e3256ec7ff5bcb18
Senih BAYANKULU <senih.bayankulu25@ogr.atauni.edu.tr>
2026-09-15 20:27:57 +0300

    Fix Season 2 Level 5 JavaScript integrity

--- Season-2/Level-5/code.js
+++ Season-2/Level-5/code.js
@@ -15,6 +15,10 @@ var CryptoAPI = (function() {
 			size: 20,
 			block: 64,
 			hash: function(s) {
+				if (typeof s !== "string") {
+					throw new TypeError("CryptoAPI.sha1.hash() expects a string");
+				}
+
 				var len = (s += '\x80').length,
 					blocks = len >> 6,
 					chunk = len & 63,
@@ -22,8 +26,17 @@ var CryptoAPI = (function() {
 					i = 0,
 					j = 0,
 					H = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0],
-					w = [];
-					
+					w = [
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+					];
+
 				while (chunk++ != 56) {
 					s += "\x00";
 					if (chunk == 64) {
@@ -31,28 +44,30 @@ var CryptoAPI = (function() {
 						chunk = 0;
 					}
 				}
-				
+
 				for (s += "\x00\x00\x00\x00", chunk = 3, len = 8 * (len - 1); chunk >= 0; chunk--) {
 					s += encoding.b2a(len >> (8 * chunk) & 255);
 				}
-					
+
 				for (i = 0; i < s.length; i++) {
 					j = (j << 8) + encoding.a2b(s[i]);
 					if ((i & 3) == 3) {
 						w[(i >> 2) & 15] = j;
 						j = 0;
 					}
-					if ((i & 63) == 63) CryptoAPI.sha1._round(H, w);
+					if ((i & 63) == 63) internalRound(H, w);
 				}
-				
+
 				for (i = 0; i < H.length; i++)
 					for (j = 3; j >= 0; j--)
 						res += encoding.b2a(H[i] >> (8 * j) & 255);
 				return res;
-			}, // End "hash"
+			},
 			_round: function(H, w) { }
-		} // End "sha1"
-	}; // End "API"
+		}
+	};
 
-	return API; // End body of anonymous function
-})(); // End "CryptoAPI"
+	var internalRound = API.sha1._round;
+
+	return API;
+})();
